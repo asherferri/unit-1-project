03d6867534a46df4cf7b9bf7365f0ff71dbb9c36
Asher Ferri <asher@Ashers-MBP.fios-router.home>
2020-07-24 06:35:28 -0400

    game object, hardcoded locations

--- game/main.js
+++ game/main.js
@@ -6,3 +6,16 @@ const myH1 = document.querySelector('h1')
 myH1.innerText = 'FLing potatoes of the sky'
 
 //couple of sleep hrs, before puttin in code what i got on paper...
+
+const warZone = {
+	grid: 7,
+	attackFleet: 3,
+	aircraftSize: 3,
+	aircraftDown: 0,
+//start coordinates
+	enemyFleet: [
+		{ coordinates: ["43", "53", "63"], kaputs: ["", "", ""] },
+		{ coordinates: ["15", "25", "35"], kaputs: ["", "", ""] },
+		{ coordinates: ["10", "11", "12"], kaputs: ["", "", ""] }
+	],
+}
