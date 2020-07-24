a0e4d570708f8403ff04ca8042f59a027f078e80
Asher Ferri <asher@Ashers-MBP.fios-router.home>
2020-07-23 21:58:27 -0400

    sleep deprived, gonna singn off for 3 hrs...

--- game/main.js
+++ game/main.js
@@ -1,1 +1,8 @@
 console.log("it's hooked!!!")
+//testing dom destruction...
+const myBody = document.querySelector('body')
+console.log(myBody)
+const myH1 = document.querySelector('h1')
+myH1.innerText = 'FLing potatoes of the sky'
+
+//couple of sleep hrs, before puttin in code what i got on paper...
